0636942fd51f9d371811e11d937f6b80bfd0a546
Dominik Steinmann <dominik.steinmann@students.fhnw.ch>
2020-03-02 17:26:31 +0100

    first commit

---
+++ src/main/java/bank/local/ConprBankLauncher.java
@@ -0,0 +1,13 @@
+package bank.local;
+
+import bank.gui.ClientFX;
+
+/**
+ * Launcher for the ConprBank.
+ */
+public class ConprBankLauncher {
+	public static void main(String[] args) {
+		ClientFX.main(new String[] { ConprBankDriver.class.getName() });
+	}
+}
+  
